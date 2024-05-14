99263c6e00b90249b19fe1fbd4536ccedead0df6
IamToef <tranphong14204@gmail.com>
2024-05-14 20:38:45 +0700

    commit changes

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
+import socket
 import uvicorn
+import webbrowser
 
-if __name__=="__main__":
-    uvicorn.run("app:app", host="192.168.1.8", port=1000, reload=True)
+if __name__ == "__main__":
+    host = socket.gethostbyname(socket.gethostname())
+    web_url = f"http://{host}:8000/docs"  # URL của trang tài liệu FastAPI
+    webbrowser.open(web_url)  # Tự động mở trình duyệt với URL
+
+    uvicorn.run("app:app", host=host, port=8000, reload=True)
